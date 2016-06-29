390cc701c086955fc04fb248a4c3e176db14f7ba
Kevin Dong <kevindong@users.noreply.github.com>
2016-06-29 19:59:10 -0400

    Added email validatino

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :microposts
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/, message: "address is not valid" }
   validates :name, presence: true
 end
